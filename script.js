274462704358dd1cccca9f6abc200f5ea59fb4d2
Joseph <61471959+Pineapplecookiemonster@users.noreply.github.com>
2025-01-01 22:49:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("movies.json")
+    fetch("movies22.json")
         .then(response => response.json())
         .then(data => {
             populateTable(data);
